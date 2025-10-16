3300310f9863c68ed07d0f984b3341abc98c99dc
nazmul.nahid <nazmul.nahid@pathao.com>
2025-10-16 17:19:16 +0600

    initial commit

---
+++ app/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <AppAlert>
+      This is an auto-imported component
+    </AppAlert>
+  </div>
+</template>
+
+<style>
+h1 {
+  font-family: 'FarAwayGalaxy', sans-serif;
+}
+</style>
